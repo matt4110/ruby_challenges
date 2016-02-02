0ed81b3f3d9f842e0919ff4bb6334b61e287fb9d
Matthew Johnson <matthew.johnson4110@gmail.com>
2016-02-02 11:08:21 -0500

    Added blank_file.rb

---
+++ blank_file.rb
@@ -0,0 +1,12 @@
+puts "Hello, world!"
+
+puts "Isn't GIT awesome?"
+user_answer = gets.chomp
+
+if user_answer == "yes"
+	puts "Me Too!"
+elsif user_answer == "y"
+	puts "Me Too!"
+else
+	puts "Oh...well I really like it..."
+end
